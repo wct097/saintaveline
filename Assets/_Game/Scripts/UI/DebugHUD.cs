da5be1c6b267a6628836d832e7bf6095c07e4afa
Addy <aclaure@gmail.com>
2025-11-06 07:24:22 -0500

    cleaning up the debug script;

--- Assets/_Game/Scripts/UI/DebugHUD.cs
+++ Assets/_Game/Scripts/UI/DebugHUD.cs
@@ -5,33 +5,14 @@ using UnityEngine;
 
 public class DebugHUD : MonoBehaviour
 {
-    // UI Elements
-    public GameObject hudPanel;
-    public TextMeshProUGUI distanceText;
-    public TextMeshProUGUI sonNPCStateText;
-    public TextMeshProUGUI sonNPCDistanceText;
-    public TextMeshProUGUI enemyHealthText;
-    public TextMeshProUGUI boatText;
+    [Header("Object to measure distance to")]
+    [SerializeField] private TextMeshProUGUI distanceText;
+    [SerializeField] private GameObject distanceObject;
 
-    // Objects of interest
-    public CharacterController controller;
-    public GameObject enemyNPC;
-    private GameEntity enemyNPCHealth;
-    public SonNPC sonNPC;
-    public BoatWaterDetector boatDetector;
-
-    public GameObject distanceObject;
-
-    private bool isVisible = true;
     private Transform _playerTransform;
     
     void Start()
     {
-        if (enemyNPC)
-        {
-            enemyNPCHealth = enemyNPC.GetComponent<GameEntity>();
-        }
-
         var player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
@@ -44,57 +25,12 @@ public class DebugHUD : MonoBehaviour
         // Toggle visibility with "."
         if (Input.GetKeyDown(KeyCode.Period))
         {
-            isVisible = !isVisible;
-            hudPanel.SetActive(isVisible);
+            this.enabled = !this.enabled;
         }
 
-        if (isVisible)
-        {
-            float distanceValue = Vector3.Distance(_playerTransform.position, distanceObject.transform.position);
-            distanceText.text = "Distance: " + distanceValue.ToString("F2");
+        if (!this.enabled) return;
 
-
-            //            sonNPCStateText.text = "SonNPC State: " + sonNPC.StateMachine.CurrentState?.GetType().Name;
-            //            float distance = Vector3.Distance(controller.transform.position, sonNPC.transform.position);
-            //            sonNPCDistanceText.text = "SonNPC Dist: " + distance.ToString("F2");
-
-            //            // As we now have multiple enemies, we should either extend the DebugHUD to support multiple enemies
-            //            // Or simply remove the enemy health display if not needed
-            //            // This object check prevents null reference exceptions when we don't have an enemy assigned
-            //            if (enemyNPC != null && enemyNPCHealth != null)
-            //            {
-            //                enemyHealthText.text = "Enemy Health: " + enemyNPCHealth.Health.ToString("F2");
-            //            }
-
-            //            string boatLandTest = boatDetector.IsOverland ? "Overland" : "Not Overland";
-            //            string boatWaterTest = boatDetector.IsOnWater ? "On Water" : "Not On Water";
-            //            string boatBeachedTest = boatDetector.IsBeached ? "Beached" : "Not Beached";
-
-            //            boatText.text = $@"Boat Land: {boatLandTest}
-            //Boat Water: {boatWaterTest}
-            //Boat Beached: {boatBeachedTest}
-            //AvgWaterDepth: {boatDetector.AvgWaterDepth}
-            //WaterCoverage01: {boatDetector.WaterCoverage01}
-            //MinGroundClearance: {boatDetector.MinGroundClearance}
-            //WaterHits: {boatDetector.WaterHits}
-            //SamplePoints:
-            //{SamplePointsString()}
-            //";
-        }
+        float distanceValue = Vector3.Distance(_playerTransform.position, distanceObject.transform.position);
+        distanceText.text = "Distance: " + distanceValue.ToString("F2");
     }
-
-    string SamplePointsString()
-    {
-        if (boatDetector.SamplePoints == null) return string.Empty;
-        string s = "";
-        for (int i = 0; i < boatDetector.SamplePoints.Length; i++)
-        {
-            if (boatDetector.SamplePoints[i] != null)
-            {
-                s += boatDetector.SamplePoints[i].name + ":" + boatDetector.SamplePoints[i].position.ToString("F2") + "\n";
-            }
-        }
-        return s;
-    }
-
 }
