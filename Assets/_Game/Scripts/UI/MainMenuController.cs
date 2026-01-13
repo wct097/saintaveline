d4b2504361b1c60ccbfc038921c832141a471490
Will Tyler <will.tyler@gmail.com>
2026-01-12 19:13:54 -0500

    fix(phase0): Task 1 - MainMenuController runtime button wiring

--- Assets/_Game/Scripts/UI/MainMenuController.cs
+++ Assets/_Game/Scripts/UI/MainMenuController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -7,6 +8,20 @@ public class MainMenuController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+
+        // Wire PlayButton
+        var playBtn = GameObject.Find("PlayButton")?.GetComponent<Button>();
+        if (playBtn != null)
+            playBtn.onClick.AddListener(OnNewGameClicked);
+        else
+            Debug.LogWarning("MainMenuController: PlayButton not found");
+
+        // Wire ExitButton
+        var exitBtn = GameObject.Find("ExitButton")?.GetComponent<Button>();
+        if (exitBtn != null)
+            exitBtn.onClick.AddListener(OnQuitClicked);
+        else
+            Debug.LogWarning("MainMenuController: ExitButton not found");
     }
 
     public void OnNewGameClicked()
