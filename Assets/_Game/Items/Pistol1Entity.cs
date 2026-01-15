wct097/saintaveline

commit e638f13349834548b74a6725b87e0ec69130b51a

starting to take into account a moving npc when aiming the gun;

Assets/_Game/Items/Pistol1Entity.cs
@@ -120,20 +120,15 @@ public class Pistol1Entity : ItemEntity
         _attackCoroutine = null;
     }
 
-    private Vector3 GetFireDirection()
+    void Shoot()
     {
         Assert.IsNotNull(this.OwnerEntity, "Pistol1Entity.GetFireDirection: OwnerEntity is null.");
         Assert.IsNotNull(this.OwnerEntity as BaseNPC, "Pistol1Entity.GetFireDirection: OwnerEntity is not a CharacterEntity.");
 
         // purposely recalculate `npcOwner` since the target can change between shots
         BaseNPC? npcOwner = this.OwnerEntity as BaseNPC;
-        return npcOwner!.DirectionToTarget(true);
-    }
+        Vector3 direction = npcOwner!.DirectionToTarget(true);
 
-    void Shoot()
-    {
-        Vector3 direction = GetFireDirection();
-        
         if (Physics.Raycast(_firePoint!.position, direction, out RaycastHit hit, _pistolItemData!.FireRange))
         {
             var entity = hit.collider.GetComponent<GameEntity>();

Assets/_Game/Scenes/Game.unity
@@ -2634,7 +2634,7 @@ PrefabInstance:
       objectReference: {fileID: 0}
     - target: {fileID: 5490736754747808725, guid: 2a405954f53c9634482128343354e0fa, type: 3}
       propertyPath: m_AnchoredPosition.y
-      value: 2221241
+      value: 2263195
       objectReference: {fileID: 0}
     - target: {fileID: 5798461712433099780, guid: 2a405954f53c9634482128343354e0fa, type: 3}
       propertyPath: m_Enabled
@@ -2710,7 +2710,7 @@ PrefabInstance:
       objectReference: {fileID: 0}
     - target: {fileID: 8497853822552326300, guid: 2a405954f53c9634482128343354e0fa, type: 3}
       propertyPath: m_AnchoredPosition.y
-      value: 3173006
+      value: 3229836.2
       objectReference: {fileID: 0}
     - target: {fileID: 8827480874876668751, guid: 2a405954f53c9634482128343354e0fa, type: 3}
       propertyPath: m_AnchorMax.x
@@ -16467,15 +16467,15 @@ PrefabInstance:
       objectReference: {fileID: 0}
     - target: {fileID: 5311034482893019840, guid: bd2794bf9e64d463991405af546f3cc4, type: 3}
       propertyPath: m_LocalPosition.x
-      value: 488.5
+      value: 485.599
       objectReference: {fileID: 0}
     - target: {fileID: 5311034482893019840, guid: bd2794bf9e64d463991405af546f3cc4, type: 3}
       propertyPath: m_LocalPosition.y
-      value: 1.8
+      value: 2.211
       objectReference: {fileID: 0}
     - target: {fileID: 5311034482893019840, guid: bd2794bf9e64d463991405af546f3cc4, type: 3}
       propertyPath: m_LocalPosition.z
-      value: 509.6
+      value: 512.81
       objectReference: {fileID: 0}
     - target: {fileID: 5311034482893019840, guid: bd2794bf9e64d463991405af546f3cc4, type: 3}
       propertyPath: m_LocalRotation.w

Assets/_Game/Scripts/EnemyNPCs/EnemyAttackState.cs
@@ -7,9 +7,6 @@ using System.Collections.Generic;
 [NPCStateTag("EnemyAttack")]
 public class EnemyAttackState : NPCState
 {  
-    public Transform _firePoint = new GameObject("FirePoint").transform;
-    public AudioClip[] _gunshotSounds;
-
     public float fireRate = 1f;
     public float range = 50f;
     public float damage = 10f;
@@ -19,7 +16,7 @@ public class EnemyAttackState : NPCState
     [Tooltip("Audio clip to play when the gun is fired.")]
     private AudioSource _audioSource;
 
-    private float nextFireTime = 0f;
+    private float _nextFireTime = 0f;
     private LineRenderer _lineRenderer;
     private readonly GameEntity _targetEntity;
 
@@ -30,11 +27,6 @@ public class EnemyAttackState : NPCState
         {
             throw new System.Exception("BaseNPC is not an EnemyNPC. Cannot enter attack state.");
         }
-
-        _firePoint.SetParent(this.NPC!.transform);
-        _firePoint.localPosition = new Vector3(0.004f, 0.6019999f, 0.425f);
-        _firePoint.localRotation = Quaternion.Euler(0f, 0f, 0f);
-        _firePoint.localScale = new Vector3(1f, 0.5555556f, 1f);
 
         _lineRenderer = this.NPC!.GetComponent<LineRenderer>();
         _lineRenderer.enabled = false;
@@ -55,12 +47,6 @@ public class EnemyAttackState : NPCState
         _audioSource.playOnAwake = false;
         _audioSource.SetCustomCurve(AudioSourceCurveType.CustomRolloff, rolloff);
         _audioSource.maxDistance = 50f; // Can tweak based on how far you want it heard
-
-        _gunshotSounds = new AudioClip[]
-        {
-            Resources.Load<AudioClip>("Sounds/gunshot1"),
-            Resources.Load<AudioClip>("Sounds/gunshot2")
-        };
 
         _targetEntity = target;
     }
@@ -103,49 +89,15 @@ public class EnemyAttackState : NPCState
             );
         }
 
-        if (Time.time >= nextFireTime)
+        if (Time.time >= _nextFireTime)
         {
             this.NPC!.EquippedItem?.Attack();
-            //Shoot();
+
             // get a random time between 0.5 and 1.5 seconds
             float randomTime = UnityEngine.Random.Range(0.5f, 1.5f);
-            nextFireTime = Time.time + randomTime;
+            _nextFireTime = Time.time + randomTime;
         }
 
         return null;
-    }
-
-    void Shoot()
-    {
-        var direction = this.NPC!.Target.position - _firePoint.position;
-        if (Physics.Raycast(_firePoint.position, direction, out RaycastHit hit, range))
-        {
-            this.NPC!.StartCoroutine(FireRayEffect(hit.point));
-
-            // get the distance from the fire point to the hit point
-            float distance = Vector3.Distance(_firePoint.position, hit.point);
-            int damage = Mathf.RoundToInt(defaultDamage * (1 - (distance / range)));
-
-            var targetEntity = hit.collider.GetComponent<GameEntity>();
-            targetEntity?.TakeDamage(damage);
-        }
-        else
-        {
-            this.NPC!.StartCoroutine(FireRayEffect(_firePoint.position + direction * range));
-        }
-
-        _audioSource.PlayOneShot(_gunshotSounds[UnityEngine.Random.Range(0, _gunshotSounds.Length)]);
-    }
-
-    IEnumerator FireRayEffect(Vector3 hitPoint)
-    {
-        _lineRenderer.SetPosition(0, _firePoint.position);
-        _lineRenderer.SetPosition(1, hitPoint);
-        _lineRenderer.enabled = true;
-
-        yield return new WaitForSeconds(0.05f);
-
-        _lineRenderer.enabled = false;
-        // // OnGunFired?.Invoke();
     }
 }

Assets/_Game/Scripts/EnemyNPCs/EnemyPursueState.cs
@@ -8,10 +8,10 @@ public class EnemyPursueState : NPCState
     private UnityEngine.AI.NavMeshAgent? _agent = null;
     private AudioClip? _warningSound;
     private AudioClip? _willFindYouSound;
+    private float _nextFireTime = 0f;
 
     private readonly GameEntity _targetEntity;
     
-
     // TODO: this is a poor man's way to stop chasing, eventually we will want to be a 
     // little smarter -- for example, if the NPC cannot "see" the Target, then the NPC could
     // go to the last position it saw the Target, and if the Target is not in range or
@@ -95,6 +95,15 @@ public class EnemyPursueState : NPCState
             // Target is out of range, go back to idle state which we pushed earlier
             return new NPCStateReturnValue(
                 NPCStateReturnValue.ActionType.PopState);
+        }
+
+        if (Time.time >= _nextFireTime)
+        {
+            this.NPC!.EquippedItem?.Attack();
+
+            // get a random time between 0.5 and 1.5 seconds
+            float randomTime = UnityEngine.Random.Range(0.5f, 1.5f);
+            _nextFireTime = Time.time + randomTime;
         }
 
         return null;

commit 0fabf69a0841693ae0014202e85b0fb53fedd3a9

fix: Restore player shooting broken by e638f133
Commit e638f133 replaced camera-based aiming with BaseNPC.DirectionToTarget(),
which crashes for player (CharacterEntity, not BaseNPC).

Co-Authored-By: Claude Opus 4.5 <noreply@anthropic.com>

Assets/_Game/Items/Pistol1Entity.cs
@@ -122,12 +122,21 @@ public class Pistol1Entity : ItemEntity
 
     void Shoot()
     {
-        Assert.IsNotNull(this.OwnerEntity, "Pistol1Entity.GetFireDirection: OwnerEntity is null.");
-        Assert.IsNotNull(this.OwnerEntity as BaseNPC, "Pistol1Entity.GetFireDirection: OwnerEntity is not a CharacterEntity.");
+        Assert.IsNotNull(this.OwnerEntity, "Pistol1Entity.Shoot: OwnerEntity is null.");
 
-        // purposely recalculate `npcOwner` since the target can change between shots
+        // Get fire direction based on owner type
+        Vector3 direction;
         BaseNPC? npcOwner = this.OwnerEntity as BaseNPC;
-        Vector3 direction = npcOwner!.DirectionToTarget(true);
+        if (npcOwner != null)
+        {
+            // NPC: aim at their target
+            direction = npcOwner.DirectionToTarget(true);
+        }
+        else
+        {
+            // Player: aim where the camera is looking
+            direction = _mainCamera!.transform.forward;
+        }
 
         if (Physics.Raycast(_firePoint!.position, direction, out RaycastHit hit, _pistolItemData!.FireRange))
         {